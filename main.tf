ce4e70ab2301759029fe2d3a18a237b37a3112cb
stackitgit <53795294+stackitgit@users.noreply.github.com>
2021-05-19 05:42:58 -0400

    Main.tf update

--- main.tf
+++ main.tf
@@ -371,7 +371,7 @@ resource "aws_route53_record" "www" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = "www.${data.aws_route53_zone.selected.name}"
   type    = "A"
-  ttl     = "300"
+  //ttl     = "300"
   //records = ["10.0.0.1"]
   //records = [aws_alb.stack-alb.public_ip]
    alias {
