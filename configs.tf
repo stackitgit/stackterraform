61d8f4d543dfa83f71f0d4401f7f7af0b4752e6e
stackitgit <53795294+stackitgit@users.noreply.github.com>
2021-05-19 11:11:18 -0400

    AWS SSM

--- configs.tf
+++ configs.tf
@@ -12,6 +12,34 @@ data "template_file" "bootstrap" {
     }
 }
 
+data "aws_secretsmanager_secret_version" "creds" {
+  # Fill in the name you gave to your secret
+  secret_id = "creds"
+}
+
+locals {
+  db_creds = jsondecode(
+    data.aws_secretsmanager_secret_version.creds.secret_string
+  )
+}
+
+data "template_file" "wp-config" {
+  template = file(format("%s/config/wp-config.php", path.module))
+  vars = {
+    db_username=local.db_creds.username
+    db_password=local.db_creds.password
+    db_name=local.db_creds.db_name
+    db_host=var.DB_HOST
+  }
+}
+
+resource "aws_ssm_parameter" "wp-config-parameter" {
+  name      = "${local.ssm_parameter_path}/var/www/html/wp-config.php"
+  type      = "SecureString"
+  value     = base64encode(data.template_file.nginx.rendered)
+  overwrite = "true"
+}
+
 /*
 data "template_file" "wp-config.php" {
     template = file(format("%s/config/wp-config.php", path.module))
