f235278570ca83fc1d6bf006b5589bed2b643a0c
stackitgit <53795294+stackitgit@users.noreply.github.com>
2021-05-19 11:14:02 -0400

    AWS SSM

--- configs.tf
+++ configs.tf
@@ -12,10 +12,6 @@ data "template_file" "bootstrap" {
     }
 }
 
-data "aws_secretsmanager_secret_version" "creds" {
-  # Fill in the name you gave to your secret
-  secret_id = "creds"
-}
 
 locals {
   db_creds = jsondecode(
